0d6e310755869305d0f9f7cf1f3a766d4f3d3085
Zunair Pervaiz <86952058+zunairpervaiz@users.noreply.github.com>
2022-10-25 17:39:48 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: "REST API Tutorial",
+      title: "Quick Chats",
       //home: UsersApp(),
     );
   }
